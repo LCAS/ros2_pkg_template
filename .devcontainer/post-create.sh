2fb4cf7d38bfe81d43bb68bbc88d9db9ea85be1c
Marc Hanheide <marc@hanheide.net>
2025-01-28 21:45:51 +0000

    Enhance post-create script with error handling and new aliases

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -xe
+
 
 function add_config_if_not_exist {
     if ! grep -F -q "$1" $HOME/.bashrc; then
@@ -9,11 +11,17 @@ function add_config_if_not_exist {
 
 add_config_if_not_exist "source /opt/ros/humble/setup.bash"
 add_config_if_not_exist "source /opt/ros/lcas/install/setup.bash"
+add_config_if_not_exist "alias rviz_sensors='rviz2 -d /opt/ros/lcas/install/limo_description/share/limo_description/rviz/model_sensors_real.rviz'"
+add_config_if_not_exist "alias tidybot_sim='ros2 launch uol_tidybot tidybot.launch.py'"
 
-LOCAL_SETUP_FILE=`pwd`/install/setup.bash
-add_config_if_not_exist "if [ -r $LOCAL_SETUP_FILE ]; then source $LOCAL_SETUP_FILE; fi"
 
 source /opt/ros/humble/setup.bash
 source /opt/ros/lcas/install/setup.bash
 
-colcon build --symlink-install --continue-on-error
+colcon build --symlink-install --continue-on-error || true
+
+LOCAL_SETUP_FILE=`pwd`/install/setup.bash
+add_config_if_not_exist "if [ -r $LOCAL_SETUP_FILE ]; then source $LOCAL_SETUP_FILE; fi"
+
+sleep 10
+DISPLAY=:1 xfconf-query -c xfce4-desktop -p $(xfconf-query -c xfce4-desktop -l | grep "workspace0/last-image") -s /usr/share/backgrounds/xfce/lcas.jpg  || true
